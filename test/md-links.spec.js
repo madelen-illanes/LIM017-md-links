3d051f4c40a573d461f22e48761dd4152788fc92
madelen <madelen2811@gmail.com>
2022-05-17 19:53:38 -0500

    config babel

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,22 +1,12 @@
-import {transformToAbsolutePath,} from '../src/mdlinks.js'
+import {transformToAbsolutePath} from './md-links.js';
 
 const routeTest = 'C:\Users\Madelen\LIM017-md-links\documents\file1.md'
 
 describe('transformToAbsolutePath',()=>{
  it('show resolve path in windows', () =>{
-   expect(transformToAbsolutePath('documents\file1.md')).toBe(routeTest)
+   expect(transformToAbsolutePath('documents\file1.md')).toBe(routeTest);
  })
 });
 
 
 
-
-
-
-describe('mdLinks', () => {
-
-  it('should...', () => {
-    console.log('FIX ME!');
-  });
-
-});
