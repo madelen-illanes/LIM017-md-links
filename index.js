bfea549ee7fb32cc9845e988cffb928b120cc396
madelen <madelen2811@gmail.com>
2022-06-01 02:43:19 -0500

    package

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-/* eslint-disable semi */
-/* eslint-disable object-curly-newline */
-// eslint-disable-next-line linebreak-style
 import { transformToAbsolutePath, getFileMd, readFile, getObject } from './md-links.js'
-// eslint-disable-next-line linebreak-style
 
- const mdLinks = (path, options) => {
-    return new Promise((resolve, reject) => {
-        const convertThepath = transformToAbsolutePath(path) //Función que convierte ruta en absoluta
-        console.log('convertThepath', convertThepath);
-        const arrayFileMd = getFileMd(convertThepath) // Si es archivo md
-        console.log('arrayFileMd', arrayFileMd);
-        getObject(arrayFileMd)
-        .then((res) => {
-        if (options.validate !== true){
-        resolve(readFile(convertThepath));
-        } else {
-        resolve(getObject(convertThepath));
-        }
-        })
-        .catch((error) => {
-        reject(error);
-        });
+    const mdLinks = (path, options) => new Promise((resolve, reject) => {
+        const convertThepath = transformToAbsolutePath(path);
+       
     })
-};
-//mdLinks().then((resolve) => console.log(resolve));
+
+
+
+// mdLinks().then((resolve) => console.log(resolve));
+
+
+
+const arrayFileMd = getFileMd(convertThepath) // Si es archivo md
+console.log('arrayFileMd', arrayFileMd);
+getObject(arrayFileMd)
+.then((res) => {
+if (options.validate !== true){
+resolve(readFile(convertThepath));
+} else {
+resolve(getObject(convertThepath));
+}
+})
+.catch((error) => {
+reject(error);
+});
